efa2134ac4243c97b783f2ac843522bbc75d2282
Amyharrison93 <amycantflynow@gmail.com>
2023-09-03 22:43:11 +0100

    fdas

--- MechLighting/Games.py
+++ MechLighting/Games.py
@@ -1,17 +1,20 @@
 import RPi.GPIO as GPIO
 import ImagePreProcess as imgGet
 import damageDetection as dmgDetect
+healthLED = 23
+damageLED = 18
 
 def ArmoredCore6(cam):
     frame = imgGet.ScaleAndCaptureFrame(cam)
     healthBar = imgGet.CropFrameHealth(frame)
-    health = dmgDetect.DetectHealth(healthBar, 23)
-    dmgTaken = dmgDetect.DetectDamage(healthBar)
+    stunBar = imgGet.CropFrameStun(frame)
 
+    health = dmgDetect.DetectHealth(healthBar, healthLED)
+    dmgTaken = dmgDetect.DetectDamage(healthBar)
     if(dmgTaken > 0):
-        GPIO.output(18, 1)
+        GPIO.output(damageLED, 1)
     else:
-        GPIO.output(18, 0)
+        GPIO.output(damageLED, 0)
         
     print("damage taken = ", dmgTaken)
     print("health remaining: ", health)
